30de94829d4ff7148f2d4d234e473ae680ae4ae8
keshavsoft <nknnkumar@yahoo.com>
2026-08-19 14:58:10 +0530

    foot 5

--- buildTable/forFooter/index.js
+++ buildTable/forFooter/index.js
@@ -1,8 +1,8 @@
-import runMaxVersion from "./v4/index.js";
+import runMaxVersion from "./v5/index.js";
 // import { buildTableBody as buildTableBodyV2 } from "./v2/TableBody.js";
 
 // Export the version object so consumers can explicitly select a version if needed
-export const v4 = { buildTableSummary: runMaxVersion };
+export const v5 = { buildTableSummary: runMaxVersion };
 // export const v2 = { buildTableBody: buildTableBodyV2 };
 
 // Default export uses v2 since it has the new features
