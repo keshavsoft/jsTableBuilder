12cc62939a5280caf75b7a11f497e06124853088
keshavsoft <nknnkumar@yahoo.com>
2026-08-20 14:51:27 +0530

    comp 3

--- webComponents/v4/cellRenderers/renderInputControl/index.js
+++ webComponents/v4/cellRenderers/renderInputControl/index.js
@@ -1,9 +1,9 @@
-import runMaxVersion from "./v2/index.js";
+import runMaxVersion from "./v3/index.js";
 // import { buildTableBody as buildTableBodyV2 } from "./v2/TableBody.js";
 
 // Export the version object so consumers can explicitly select a version if needed
 export const v1 = { renderInputControl: runMaxVersion };
-// export const v2 = { buildTableBody: buildTableBodyV2 };
+export const v2 = { renderInputControl: runMaxVersion };
 
 // Default export uses v2 since it has the new features
 export const renderInputControl = runMaxVersion;
