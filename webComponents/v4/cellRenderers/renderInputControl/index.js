384c499bbda2922a3766f08a312bf3f303cdd731
keshavsoft <nknnkumar@yahoo.com>
2026-08-20 15:18:49 +0530

    v5

--- webComponents/v4/cellRenderers/renderInputControl/index.js
+++ webComponents/v4/cellRenderers/renderInputControl/index.js
@@ -2,12 +2,14 @@ import runV1 from "./v1/index.js";
 import runV2 from "./v2/index.js";
 import runV3 from "./v3/index.js";
 import runV4 from "./v4/index.js";
+import runV5 from "./v5/index.js";
 
 // Export the version object so consumers can explicitly select a version if needed
 export const v1 = { renderInputControl: runV1 };
 export const v2 = { renderInputControl: runV2 };
 export const v3 = { renderInputControl: runV3 };
 export const v4 = { renderInputControl: runV4 };
+export const v5 = { renderInputControl: runV5 };
 
-// Default export uses v4 since it has the new features
-export const renderInputControl = runV4;
+// Default export uses v5 since it has the new features
+export const renderInputControl = runV5;
