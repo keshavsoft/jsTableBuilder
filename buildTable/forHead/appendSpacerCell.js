b882c59aee9000d68aa6591a7f4f315af389d112
keshavsoft <nknnkumar@yahoo.com>
2026-08-23 08:09:20 +0530

    vertical

--- buildTable/forHead/appendSpacerCell.js
+++
@@ -1,5 +0,0 @@
-export function appendSpacerCell({ inHeaderRowElement, inCellClass }) {
-    const spacerCell = document.createElement("th");
-    if (inCellClass) spacerCell.className = inCellClass;
-    inHeaderRowElement.appendChild(spacerCell);
-}
--- buildTable/utils/style/normalizeSize.js
+++
@@ -1,8 +0,0 @@
-export function normalizeSize(inValue) {
-    if (inValue !== undefined && inValue !== null && inValue !== "") {
-        if (typeof inValue === 'number' || /^\d+$/.test(String(inValue).trim())) {
-            return `${inValue}px`;
-        }
-    }
-    return inValue;
-}
