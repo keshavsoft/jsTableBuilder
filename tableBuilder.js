238564a4950dad98b9fce4fc08b38002e253368a
keshavsoft <nknnkumar@yahoo.com>
2026-08-23 09:08:02 +0530

    still table need to hook

--- tableBuilder.js
+++ tableBuilder.js
@@ -1,81 +1,45 @@
-import DEFAULT_CONFIG, { DEFAULT_CLASSES } from "./buildTable/config/defaults.js";
-import mapTableOptions from "./buildTable/utils/config/mapTableOptions.js";
-import { mergeClasses } from "./buildTable/utils/config/mergeClasses.js";
-import { appendToDom } from "./buildTable/utils/dom/appendToDom.js";
-import { buildVerticalFormElements } from "./buildTable/buildVerticalForm.js";
+import { VerticalRenderer } from "./renderers/vertical/VerticalRenderer.js";
+// import { TableRenderer } from "./renderers/table/TableRenderer.js";
+import DEFAULT_CONFIG, { DEFAULT_CLASSES } from "./renderers/shared/config/defaults.js";
+import mapTableOptions from "./renderers/shared/config/mapTableOptions.js";
 
 import "./webComponents/v4/KsTableCellContent.js";
 
-const logger = {
-    showLogs: true,
-    log: function (...args) {
-        if (this.showLogs) {
-            console.log(...args);
-        }
-    }
-};
-
 class TableBuilder {
-    constructor({
-        htmlId,
-        data,
-        columns = [],
-        classes = {},
-        theme = "style1",
-        tableOptions = {},
-        verticalForm = DEFAULT_CONFIG.verticalForm,
-        rendererType = "vertical",
-        endPoints
-    }) {
-        const localHtmlId = htmlId;
-        const localData = data;
-        const localColumns = columns;
-        const localClasses = classes;
-        const localEndPoints = endPoints;
-
-        this.rendererType = rendererType;
-        this.verticalForm = verticalForm;
-
-        this.htmlId = localHtmlId;
-
-        this.classes = mergeClasses({ inClasses: localClasses, inTheme: theme });
-        
-        // Simulating dataStore for vertical form
-        this.dataStore = {
-            data: localData || [],
-            columns: localColumns || []
-        };
-        
-        this.services = {};
-        if (localEndPoints) {
-            // Setup services if needed
-        }
+    constructor(config) {
+        this.config = config;
+        this.rendererType = config.rendererType || "vertical";
+        this.htmlId = config.htmlId;
     }
 
     async appendToDom() {
-        if (this.dataStore.data.length === 0 && this.services.read) {
-            this.dataStore.originalData = await this.services.read();
-            this.dataStore.data = this.dataStore.originalData;
+        const rootElement = document.getElementById(this.htmlId);
+        if (!rootElement) {
+            console.error(`Element with id '${this.htmlId}' not found.`);
+            return;
         }
 
-        appendToDom(this);
-    };
+        rootElement.innerHTML = ""; // Clear loading state
 
-    buildVerticalFormElement() {
-        logger.log("buildVerticalFormElement called", this);
-        if (!this.verticalForm || this.verticalForm.show === false) return null;
+        let renderer;
 
-        return buildVerticalFormElements({
-            inData: this.dataStore.data,
-            inColumns: this.dataStore.columns,
-            inClasses: this.classes
-        });
-    };
+        if (this.rendererType === "vertical") {
+            renderer = new VerticalRenderer(this.config);
+        } else if (this.rendererType === "table") {
+            // renderer = new TableRenderer(this.config);
+        } else {
+            console.error(`Renderer type '${this.rendererType}' is not supported.`);
+            return;
+        }
+
+        // The specific renderer does ALL the heavy lifting
+        await renderer.build();
+    }
 
     build() {
         return this.appendToDom();
-    };
-};
+    }
+}
 
 const DEFAULT_INTERNAL_OBJECT = {
     inTableOptions: mapTableOptions(DEFAULT_CONFIG.tableOptions)
@@ -86,6 +50,6 @@ window.ks.TableBuilder = TableBuilder;
 window.ks.TableBuilder.DEFAULT_CLASSES = DEFAULT_CLASSES;
 window.ks.TableBuilder.DEFAULT_CONFIG = DEFAULT_CONFIG;
 window.ks.TableBuilder.DEFAULT_INTERNAL_OBJECT = DEFAULT_INTERNAL_OBJECT;
-window.ks.TableBuilder.version = "v10.0-renderer";
+window.ks.TableBuilder.version = "v10.1-orchestrator";
 
 export { TableBuilder, DEFAULT_CLASSES, DEFAULT_CONFIG, DEFAULT_INTERNAL_OBJECT };
