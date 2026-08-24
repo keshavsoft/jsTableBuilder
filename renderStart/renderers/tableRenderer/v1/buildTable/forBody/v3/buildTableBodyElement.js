1ba66c039178e0660e683e10a22872acb1626908
Keshav Nalam <nknnkumar@yahoo.com>
2026-08-24 11:44:04 +0530

    table design working on

--- renderStart/renderers/tableRenderer/v1/buildTable/forBody/v3/buildTableBodyElement.js
+++
@@ -1,7 +0,0 @@
-export function buildTableBodyElement({ inWrapperClass }) {
-    const bodyWrapperElement = document.createElement("tbody");
-    
-    if (inWrapperClass) bodyWrapperElement.className = inWrapperClass;
-    
-    return bodyWrapperElement;
-}
